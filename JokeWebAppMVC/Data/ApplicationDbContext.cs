b446bcd6a6adcc7bd13ff10f7f20a953fdbc3265
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-10-13 09:23:42 +0100

    Add project files.

---
+++ JokeWebAppMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokeWebAppMVC.Models;
+
+namespace JokeWebAppMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokeWebAppMVC.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ JokeWebAppMVC/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokeWebAppMVC.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        //Constructor
+        public Joke()
+        {
+            
+        }
+    }
+}
